e832c20a98e7c155f3f1b06263f515aad530b79a
Taylor Vann <brian.t.vann@gmail.com>
2026-06-10 09:39:43 -0700

    update superaction, builds

--- examples/counter/bundled.js
+++ examples/counter/bundled.js
@@ -30266,10 +30266,9 @@ class ActionEvent extends Event {
 class SuperAction {
     #connected = false;
     #params;
-    #target;
+    #dispatch = this.#unboundDispatch.bind(this);
     constructor(params) {
-        this.#params = { ...params };
-        this.#target = params.target ?? params.host;
+        this.#params = params;
         if (this.#params.connected)
             this.connect();
     }
@@ -30291,30 +30290,35 @@ class SuperAction {
             host.removeEventListener(name, this.#dispatch);
         }
     }
-    #dispatch = this.#unboundDispatch.bind(this);
     #unboundDispatch(event) {
-        let { type, currentTarget, target } = event;
-        if (!currentTarget)
+        dispatch(this.#params, event);
+    }
+}
+function dispatch(params, event) {
+    let { host, target: dispatchTarget = host, infix = ":" } = params;
+    let { type } = event;
+    for (let target of event.composedPath()) {
+        if (!(target instanceof Element))
+            continue;
+        if (target.hasAttribute(`${type}${infix}prevent-default`))
+            event.preventDefault();
+        if (target.hasAttribute(`${type}${infix}stop-immediate-propagation`))
             return;
-        let { infix = ":" } = this.#params;
-        for (let node of event.composedPath()) {
-            if (node instanceof Element) {
-                if (node.hasAttribute(`${type}${infix}prevent-default`))
-                    event.preventDefault();
-                if (node.hasAttribute(`${type}${infix}stop-immediate-propagation`))
-                    return;
-                let actionType = node.getAttribute(`${type}${infix}`);
-                if (actionType) {
-                    let formData;
-                    if (target instanceof HTMLFormElement)
-                        formData = new FormData(target);
-                    let actionEvent = new ActionEvent({ type: actionType, target: node, event, formData }, { bubbles: true });
-                    this.#target.dispatchEvent(actionEvent);
-                }
-                if (node.hasAttribute(`${type}${infix}stop-propagation`))
-                    return;
-            }
+        let actionType = target.getAttribute(`${type}${infix}`);
+        if (actionType) {
+            let formData;
+            if (target instanceof HTMLFormElement)
+                formData = new FormData(target);
+            let actionEvent = new ActionEvent({
+                type: actionType,
+                target,
+                event,
+                formData,
+            });
+            dispatchTarget.dispatchEvent(actionEvent);
         }
+        if (target.hasAttribute(`${type}${infix}stop-propagation`))
+            return;
     }
 }
 
